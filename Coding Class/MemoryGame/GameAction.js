93a4a8bd873781e10fab6989702a9faa9934ace8
Bruce Hawthorn <55966342+goldeneagle1833@users.noreply.github.com>
2022-08-31 16:57:43 -0400

    create elements

--- Coding Class/MemoryGame/GameAction.js
+++ Coding Class/MemoryGame/GameAction.js
@@ -1,5 +1,5 @@
 const base_url = "https://api.pexels.com/v1/search";
-
+/*
 fetch("");
 
 function memoryApp() {
@@ -11,3 +11,31 @@ function memoryApp() {
     },
   });
 }
+*/
+
+function getRndInteger() {
+  return Math.floor(Math.random() * (6 - 1)) + 1;
+}
+
+ranNumA = getRndInteger();
+ranNumB = getRndInteger();
+
+let cellA = document.createElement("img");
+cellA.height = 200;
+cellA.src = `./Memory Game/${ranNumA}.png`;
+
+let cellB = document.createElement("img");
+cellB.height = 200;
+cellB.src = `./Memory Game/${ranNumB}.png`;
+
+//cellA.src = './Memory Game/7.png'
+
+let imgA = document.getElementById("divA");
+imgA.appendChild(cellA);
+
+let imgB = document.getElementById("divB");
+imgA.appendChild(cellB);
+//imgA.src = `./Memory Game/${ranNumA}.png`;
+
+//let imgB = document.getElementById("B");
+//imgB.src = `./Memory Game/${ranNumB}.png`;
